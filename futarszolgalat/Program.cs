67ebb5477870094216848ec8432b3e8b95986a68
makkos.peter <makkos.peter@students.jedlik.eu>
2025-02-07 08:56:25 +0100

    .

---
+++ futarszolgalat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace futarszolgalat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
